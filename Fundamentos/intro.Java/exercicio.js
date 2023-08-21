6e6922e421512f2ec964f845120f7b9c538c3e5e
Gabriela Marques <gabrielamaqu@gmail.com>
2023-08-21 19:41:19 -0300

    exercicio 5

--- Fundamentos/intro.Java/exercicio.js
+++ Fundamentos/intro.Java/exercicio.js
@@ -1,17 +1,9 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
 
-for (let index = 0; index < numbers.length; index +=1){
-    soma += numbers[index];
+let maiorNumero = numbers [0];
+for (let index = 1; index < numbers.length; index +=1) {
+    if (numbers[index] > maiorNumero){
+        maiorNumero = numbers[index];
+    }
 };
-
-let media = soma / numbers.length;
-
-if (media > 20) {
-    console.log('O valor da média aritmética é maior que 20');
-}
-else {
-    console.log ('O valor da média aritmética é menor ou igual a 20');
-};
-
-// console.log(media);
+console.log(maiorNumero);

f8cb09f97b6e7919ae9328674ec88de0b9f245ed
Gabriela Marques <gabrielamaqu@gmail.com>
2023-08-21 19:23:40 -0300

    exercicio 2

--- Fundamentos/intro.Java/exercicio.js
+++ Fundamentos/intro.Java/exercicio.js
@@ -1,5 +1,7 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+result = 0;
 
-for (index = 0; index < numbers.length; index +=1) {
-    console.log(numbers[index])
+for (index = 0; index < numbers.length; index += 1){ 
+    result += numbers[index];
 }
+console.log(result)

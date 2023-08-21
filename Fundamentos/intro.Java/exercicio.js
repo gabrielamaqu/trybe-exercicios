97499252cade10c9f66bae0b7087c218b1ca0794
Gabriela Marques <gabrielamaqu@gmail.com>
2023-08-21 20:02:24 -0300

    exercicio 6

--- Fundamentos/intro.Java/exercicio.js
+++ Fundamentos/intro.Java/exercicio.js
@@ -1,9 +1,17 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let num = 0   
 
-let maiorNumero = numbers [0];
-for (let index = 1; index < numbers.length; index +=1) {
-    if (numbers[index] > maiorNumero){
-        maiorNumero = numbers[index];
+
+for (let index = 0; index < numbers.length; index +=1){
+    if (numbers[index] % 2 !==0){
+
+        num +=1;
     }
+
+}
+if (num === 0){
+    console.log ('Nenhum valor impar encontrado');
+} 
+else {
+    console.log (num);
 };
-console.log(maiorNumero);

2c4f1126bf36561ddfc5956d298d1d47e510caf3
Gabriela Marques <gabrielamaqu@gmail.com>
2023-08-21 20:09:10 -0300

    desafio fatorial

--- Fundamentos/intro.Java/exercicio.js
+++ Fundamentos/intro.Java/exercicio.js
@@ -1,17 +1,6 @@
-const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let num = 0   
+let fator = 10;
 
-
-for (let index = 0; index < numbers.length; index +=1){
-    if (numbers[index] % 2 !==0){
-
-        num +=1;
-    }
-
-}
-if (num === 0){
-    console.log ('Nenhum valor impar encontrado');
-} 
-else {
-    console.log (num);
+for (let index = 10; index > 0;index -=1){
+    fator *= index;
 };
+console.log(fator);

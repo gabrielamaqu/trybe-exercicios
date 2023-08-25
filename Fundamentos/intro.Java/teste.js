94bdcc319d4e3c8069e65eab2705fa04443d4736
Gabriela Marques <gabrielamaqu@gmail.com>
2023-08-24 23:30:48 -0300

    exercicio feito manipulacao de objeto parte 2

--- Fundamentos/intro.Java/teste.js
+++ Fundamentos/intro.Java/teste.js
@@ -1,24 +1,60 @@
-const reader = {
-  name: 'Julia',
-  lastName: 'Pessoa',
-  age: 21,
-  favoriteBooks: [
-    {
-      title: 'O Senhor dos Anéis - a Sociedade do Anel',
-      author: 'J. R. R. Tolkien',
-      publisher: 'Martins Fontes',
-    },
-  ],
-};
-// console.log(`O livro favorito de ${reader.name} ${reader.lastName} se chama ${reader.favoriteBooks[0].title}`)
-
-reader.favoriteBooks.push(
-  {
-    title: 'Harry Potter e o Prisioneiro de Azkaban',
-    author: 'JK Rowling',
-    Publisher: 'Rocco',
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
   },
-)
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
 
-console.log(`${reader.name} tem ${reader.favoriteBooks.length} livros favoritos`)
+const customerInfo = (fullOrder) => {
+  const delivery = fullOrder.order.delivery.deliveryPerson;
+  const costumer = fullOrder.name;
+  const phoneNumber = fullOrder.phoneNumber;
+  const street = fullOrder.address.street;
+  const streetnumber = fullOrder.address.number;
+  const apnumber = fullOrder.address.apartment;
 
+  return `Olá ${delivery}, entrega para ${costumer}, Telefone: ${phoneNumber}, ${street}, ${streetnumber}, AP:${apnumber}.`
+
+}
+
+console.log(customerInfo(order));
+
+const orderModifier = (fullOrder) => {
+  fullOrder.name = 'Luiz Silva';
+  fullOrder.payment.toral = 50;
+
+  const pedido = Object.keys(fullOrder.order.pizza).join(', ');
+
+  return `Olá ${fullOrder.name}, o valor total de seu pedido de ${pedido} e ${fullOrder.order.drinks.coke.type} é R$${fullOrder.payment.total},00`;
+}
+
+console.log(orderModifier(order));

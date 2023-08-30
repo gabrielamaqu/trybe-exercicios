f345313de15865d297b14a07d9ff404748fae104
Gabriela Marques <gabrielamaqu@gmail.com>
2023-08-30 17:59:02 -0300

    exercicio feito2

--- Fundamentos/java-dom-eventos-web/script.js
+++ Fundamentos/java-dom-eventos-web/script.js
@@ -1,18 +1,4 @@
-const dad = document.getElementById('pai');
-const father = document.createElement('section')
-father.id = 'irmaos'
-dad.appendChild(father);
+// *** Remova a tag h2, filha do elemento where-are-you.
 
-const whereAreYou = document.getElementById('elementoOndeVoceEsta');
-const where = document.createElement('section');
-where.id = 'filho1'
-whereAreYou.appendChild(where);
-
-const firstChild = document.getElementById('primeiroFilhoDoFilho');
-const first = document.createElement('section');
-first.id = 'primeirofilho1'
-firstChild.appendChild(first);
-
-const paiFilho1 = first.parentElement;
-const elementoOndeVoceEsta = paiFilho1.parentElement;
-console.log(elementoOndeVoceEsta.nextElementSibling);
+const daugther = document.getElementById('where-are-you')
+daugther.removeChild(daugther.firstElementChild);

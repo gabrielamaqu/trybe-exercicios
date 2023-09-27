fb8ec7c34003e6b393fd973266c9690ee8a12e12
Gabriela Marques <gabrielamaqu@gmail.com>
2023-09-27 18:12:07 -0300

    array sort

--- Fundamentos/HTML.CSS/secao-03-intro-css/script.js
+++ Fundamentos/HTML.CSS/secao-03-intro-css/script.js
@@ -1,36 +1,19 @@
 // Exercício 1
-// O array de objetos people, contêm elementos que representam pessoas com as propriedades name e age.
-//  Utilize o método filter para filtrar as pessoas que têm a idade igual ou superior a 30.
+// A nova tarefa do seu time de desenvolvimento é organizar o sistema de pessoas colaboradoras de uma rede de mercados. Para isso:
+// Utilize o sort para ordenar o array pela idade das pessoas em ordem crescente.
 
+// Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
 
 const people = [
-    { name: 'João', age: 25 },
-    { name: 'Maria', age: 30 },
-    { name: 'Pedro', age: 20 },
-    { name: 'Ana', age: 35 },
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
   ];
 
-const result = people.filter((adult) => adult.age >= 30);
+  const orderPeople = people.sort ((a, b) => a.name.localeCompare (b.name));
 
-console.log(result)
-
-// Exercício 2
-// Considere o seguinte array de objetos que representa uma lista de jogadores de basquete:
-// Utilizando o método filter e array destructuring, crie uma função filterHighPlayers que retorna um novo array contendo 
-// apenas os jogadores com altura igual ou superior a 200 centímetros.
-
-
-
-const players = [
-    { name: 'Michael Jordan', height: 198, team: 'Chicago Bulls' },
-    { name: 'LeBron James', height: 203, team: 'Los Angeles Lakers' },
-    { name: 'Kobe Bryant', height: 198, team: 'Los Angeles Lakers' },
-    { name: 'Stephen Curry', height: 191, team: 'Golden State Warriors' },
-    { name: 'Kevin Durant', height: 211, team: 'Brooklyn Nets' },
-  ];
-
-  const filterHighPlayers = () => {
-    const result = players.filter((player) => player.height >= 200);
-    return result
-  } 
-console.log(filterHighPlayers())
+  const orderPeople = people.sort ((a, b) => b.age - a.age);
+  
+  console.log(orderPeople);
